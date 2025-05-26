e0fcf2629d8911cb9477aa56f1e3381dc7f249fb
Ariel Goldring <arigoldring77@gmail.com>
2025-05-26 15:26:53 -0400

    adjusted port to allow deployment

--- app.py
+++ app.py
@@ -98,4 +98,5 @@ def get_stats_from_form():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
